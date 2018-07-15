a7dd8a1738637d7b39a554b835fff682fe816dad
Paulo Ricardo <paulor1809@gmail.com>
2018-07-15 02:20:40 +0000

    Create function to list critical days.

--- repository.py
+++ repository.py
@@ -7,6 +7,14 @@ def connect():
     """Returns a connection with news db."""
     return psycopg2.connect("dbname=news")
 
+def execute_query(sql, params):
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, params)
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
 def list_top_articles(count):
     """Returns rank of articles."""
     sql = '''
@@ -15,20 +23,31 @@ def list_top_articles(count):
     left join log b on b.path = '/article/'||a.slug
     and b.method = 'GET'
     and status = '200 OK'
-    group by a.slug, a.title order by views desc limit %s;
+    group by a.slug, a.title 
+    order by views desc limit %s;
     '''
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(sql, (str(count),))
-    rank_list = cursor.fetchall()
-    conn.close()
-    return rank_list
+    return execute_query(sql,  (str(count),))
 
-teste = get_top_articles(3)
+def list_authors_views():
+    "Returns all views by author"
+    sql = '''
+    select count(c.id) as views, c.name
+    from articles a
+    join log b on b.path = '/article/'||a.slug
+    join authors c on c.id = a.author
+    group by c.name order by views desc 
+    '''
+    return execute_query(sql, ())
 
+def list_critical_days():
+    "Return the days that log more than 1% requests 404"
+    sql = '''
+    select error.date, error.num as error_count, success.num as success_count
+    from (select time::date as date, count(1) as num from log where status = '404 NOT FOUND' group by date) as error, 
+         (select time::date as date, count(1) as num from log where status = '200 OK' group by date) as success 
+    where error.date = success.date and ((error.num * 100)/(success.num + error.num)) >= 1   
+    '''
+    return execute_query(sql, ())
 
-print(teste[0][0])
-print(teste[1][0])
-print(teste[2][0])
-print(teste[0][1])
-print(teste[0][2])
+teste2 = list_critical_days()
+print(teste2[0][0].strftime("%Y-%m-%d"))
--- test_repository.py
+++ test_repository.py
@@ -6,15 +6,33 @@ import repository
 
 def test_list_top_articles():
     """Should return top articles in correct order"""
-    top3 = repository.get_top_articles(3)
+    top3 = repository.list_top_articles(3)
     assert len(top3) == 3
-    assert top3[0][0] == 338647 
-    assert top3[1][0] == 253801 
+    assert top3[0][0] == 338647
+    assert top3[1][0] == 253801
     assert top3[2][0] == 170098
     assert top3[0][1] == 'candidate-is-jerk'
     assert top3[0][2] == 'Candidate is jerk, alleges rival'
-    top1 = repository.get_top_articles(1)
+    top1 = repository.list_top_articles(1)
     assert len(top1) == 1
-    top9 = repository.get_top_articles(9)
+    top9 = repository.list_top_articles(9)
     assert len(top9) == 8 # There are just 8 articles
-    
+
+def test_list_authors_views():
+    """Should return top authors in correct order"""
+    authors = repository.list_authors_views()
+    assert len(authors) == 4
+    assert authors[0][0] == 507594
+    assert authors[1][0] == 423457
+    assert authors[2][0] == 170098
+    assert authors[3][0] == 84557
+    assert authors[0][1] == 'Ursula La Multa'
+    assert authors[1][1] == 'Rudolf von Treppenwitz'
+    assert authors[2][1] == 'Anonymous Contributor'
+    assert authors[3][1] == 'Markoff Chaney'
+
+def test_list_critical_days():
+    """Should return days that log more than 1% requests 404"""
+    critical_days = repository.list_critical_days()
+    assert len(critical_days) == 1
+    assert critical_days[0][0].strftime("%Y-%m-%d") == '2016-07-17'
